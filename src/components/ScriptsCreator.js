6becdcde69fc41bac37b095810f64d83cad5736e
mestupinanm <111020866+mestupinanm@users.noreply.github.com>
2025-10-08 15:48:53 -0500

    si?

--- src/components/ScriptsCreator.js
+++ src/components/ScriptsCreator.js
@@ -3,7 +3,7 @@ import { useRos } from '../contexts/RosContext';
 import { createTopic } from '../services/RosManager';
 import * as ROSLIB from 'roslib';
 import animationsTxt from "../animations/animations.txt";
-import ImagenComponent from './components/Imagen';
+import ImagenComponent from '../components/Imagen';
 
 const ScriptsCreator = () => {
     const { ros } = useRos();
